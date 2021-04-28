ae8ece03c51d0213ce1f69a85c11a792bf7ba87e
TANGhz17 <15851238039@163.com>
2021-04-28 20:20:11 +0800

    search_Fragment

---
+++ app/src/main/java/cn/tanghz17/bilidata/MainFragment.kt
@@ -0,0 +1,17 @@
+package cn.tanghz17.bilidata
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
